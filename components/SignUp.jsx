f2cb4102367321b6f96a79e7be6d0439fa250619
mostafanabil888 <sasanabil420@gmail.com>
2022-10-07 00:03:27 +0200

    final deploy

--- components/SignUp.jsx
+++ components/SignUp.jsx
@@ -10,11 +10,59 @@ export default function SignUp() {
         allYears.push(thisYear - x)
     }
     const yearList = allYears.map((x) => { return (<option key={x}>{x}</option>) });
-    //this for month 
-    const monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    
+    //this for month
+    const monthList = [
+    {
+        id : 1,
+        "month" : "January"
+    },
+    {
+        id : 2,
+        "month" : "February"
+    }, 
+    {
+        id : 3,
+        "month" : "March"
+    },
+    {
+        id : 4,
+        "month" : "April"
+    },
+    {
+        id : 5,
+        "month" : "May"
+    },
+    {
+        id : 6,
+        "month" : "June"
+    },
+    {
+        id : 7,
+        "month" : "July"
+    },
+    {
+        id : 8,
+        "month" : "August"
+    },
+    {
+        id : 9,
+        "month" : "September"
+    },
+    {
+        id : 10,
+        "month" : "October"
+    },
+    {
+        id : 11,
+        "month" : "November"
+    },
+    {
+        id : 12,
+        "month" : "December"
+    }
+    ]
     const months = monthList.map((month) => {
-        return <option key={month}>{month}</option>
+        return <option key={month.id}>{month.month}</option>
     })
  
     return (
